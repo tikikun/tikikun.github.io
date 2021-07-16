9d52aba69774adc052013ede4efca698b7275db1
daotuan <daotuan@Daos-MacBook-Pro.local>
2021-07-16 11:08:10 +0700

    update new data

--- daily_data_update.py
+++ daily_data_update.py
@@ -1,3 +1,18 @@
 import schedule
 import time
 import papermill as pm
+from git import Repo
+
+
+def update_vnexpress_data():
+    pm.execute_notebook(
+       'Update vnexpress data.ipynb',
+       'Update_data_result.ipynb'
+    )
+
+if __name__ == '__main__':
+    update_vnexpress_data()
+    repo = Repo('.')
+    repo.index.add('*')
+    repo.index.commit('update new data')
+    repo.git.push()
